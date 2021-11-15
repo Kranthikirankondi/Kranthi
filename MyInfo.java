da13c055e2b45aec15b0fea2d48f64ed54d0a1e6
kranthi <kranthikirankondi@gmail.com>
2021-11-15 11:30:59 +0530

    in

---
+++ MyInfo.java
@@ -0,0 +1,15 @@
+class MyInfo 
+{
+	public static void main(String[] args) 
+	{
+		String name = "Kranthi kiran";
+		char gen = 'M';
+		int age = 25;
+        double height = 5.11;
+        
+		System.out.println("My Name Is : "+name);
+		System.out.println("Gender : "+gen);
+		System.out.println("My Age Is :"+age);
+		System.out.println("My Height Is : "+height);
+	}
+}
